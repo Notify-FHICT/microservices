3bbf76a6455c8da9ad9b41b359d4668f1f89fc6d
Yuki Chikujoin <user.yukisnow@gmail.com>
2024-01-24 14:01:38 +0100

    rabbitmq

--- notes/main.go
+++ notes/main.go
@@ -45,7 +45,7 @@ func main() {
 
 	recordMetrics()
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@10.96.3.171:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
