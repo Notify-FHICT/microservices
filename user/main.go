174525e3a091d0177c6b61aca91d1026e6b98f80
Yuki Chikujoin <user.yukisnow@gmail.com>
2024-01-24 04:24:55 +0100

    monitoring test

--- user/main.go
+++ user/main.go
@@ -33,14 +33,14 @@ func recordMetrics() {
 	go func() {
 		for {
 			opsProcessed.Inc()
-			time.Sleep(2 * time.Second)
+			time.Sleep(3 * time.Second)
 		}
 	}()
 }
 
 var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
+	opsProcessed = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "processed_ops_3s",
+		Help: "Amount of requests made in the last 3 seconds",
 	})
 )
